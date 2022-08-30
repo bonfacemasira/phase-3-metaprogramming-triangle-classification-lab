ec5d1603b224d3ce4e47097ac262eb4ef2292a12
Bonface Masira <bonfacemasira@gmail.com>
2022-08-30 16:18:53 +0300

    commit tree 6e87930f70cffb140ae12bcda4d171b096a996fa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    validate_triangle
+   if ((side1 == side2) && (side1 == side3))
+    return :equilateral
+   elsif ((side1 == side2) || (side1 == side3) || (side2 == side3))
+    return :isosceles
+   else
+    return :scalene
+   end
+  end
+
+  def sides_greatre_than_zero?
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greatre_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
